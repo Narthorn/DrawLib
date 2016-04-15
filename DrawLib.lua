09bde318a181951fd4dc48c64ec2c1e4cb8ec30c
Narthorn <narthorn@gmail.com>
2016-04-15 14:09:10 +0200

    Add basic "default values" mechanism for style tables.
    This is actually semi-broken, since it will override any metatable
    already existing on the tStyle passed in argument. I need to figure i
    out a clean way to have cascading default values that can still be
    changed after path creation, but until then, this will do.

--- DrawLib.lua
+++ DrawLib.lua
@@ -69,7 +69,7 @@ end
 
 function DrawLib:Path(tVertices, tStyle)
 	if #self.tPaths == 0 then Apollo.RegisterEventHandler("NextFrame", "OnFrame", DrawLib) end
-	local tPath = {tVertices = tVertices, tStyle = tStyle or self.tStyle}
+	local tPath = {tVertices = tVertices, tStyle = setmetatable(tStyle or {}, {__index = self.tStyle})}
 	self.tPaths[#self.tPaths+1] = tPath
 	return tPath
 end
